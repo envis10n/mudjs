6172d216125b2ac93a730dd200c67931513f3e53
envis10n <envis10n@protonmail.com>
2019-02-08 14:12:29 -0600

    lock gossip lol

--- src/commands/gossip.js
+++ src/commands/gossip.js
@@ -3,6 +3,7 @@
     permissions: [],
     options: [],
     handler: async function(context = {}, args = [], options = {}){
+        if(!context.caller.authenticated) return "You are not logged in.";
         let message = args.join(" ");
         engine.grapevine.chat.send(context.caller.name, message, "gossip");
         engine.network.clients.broadcast({
